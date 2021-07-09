a67c5e67232f116ec6d1190d23479c9b8d8e47f3
smal1378 <smal137800@gmail.com>
2021-07-09 21:02:26 +0430

    fix bug in frequency calculator thanks to farooqkz

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def speed_checker():
     def calculate_speed():
         s = 0
         for i in range(current_d, current_d+len(d)-1):
-            s += d[(current_d+1) % len(d)] - d[current_d % len(d)]
+            s += d[(i+1) % len(d)] - d[i % len(d)]
         if s != 0:
             s = s / 10 ** 9  # change from ns to s
             clock_speed = f"{round(1 / (s / (len(d) - 1)), 2)}Hz"
         else:
             clock_speed = "N/A"
         lab.config(text=clock_speed)
-        win.after(10, calculate_speed)  # rerun the func in 50ms
+        win.after(500, calculate_speed)  # rerun the func in 50ms
     win = tkinter.Tk()
     lab = tkinter.Label(win, text="N/A")
     lab.pack()
@@ -35,12 +35,9 @@ def clock_callback():
 
 
 sleep_time = 0
-d = [0] * 50
+d = [0] * 500
 current_d = 0
 thread1 = threading.Thread(target=event_loop)
 thread2 = threading.Thread(target=speed_checker)
 thread1.start()
 thread2.start()
-
-while True:
-    pass
